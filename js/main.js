06b9762838c014905937ae4b99d0aed79af64d6a
Ethan Groene <ethangroene@gmail.com>
2022-09-16 01:34:26 -0500

    Add functionality to display search-filter results
    When searchbox is empty, the heading displaying the results disappears & all previews are shown again

--- js/main.js
+++ js/main.js
@@ -163,6 +163,9 @@ searchInput.addEventListener("keyup", (e) => {
     // Declare & assignt the event's target to a variable, which is whatever is typed into the search bar:
     let value = e.target.value.trim().toLowerCase();
 
+    let totalResults = 0;
+    const resultsMessageLocations = document.getElementsByClassName('results-message');
+
     // Check if input exists & is greater than 0:
     if (value && value.trim().length > 0) {
         const postPreviews = document.getElementsByClassName('post-preview');
@@ -173,12 +176,32 @@ searchInput.addEventListener("keyup", (e) => {
             || prev.dataset.text.toLowerCase().trim().includes(value)) {
                 prev.style.display = 'block';
                 prev.style.opacity = '1';
+                totalResults += 1;
+                for (let resultsMessage of resultsMessageLocations) {
+                    resultsMessage.style.display = 'block';
+                    resultsMessage.textContent = `Your search yielded ${totalResults} result(s).`;
+                    resultsMessage.style.margin = '4% 0';
+                    resultsMessage.style.textAlign = 'center';
+                    resultsMessage.style.color = 'var(default-text-color)';
+                }
             } else {
+                totalResults += 0;
                 prev.style.display = 'none';
+                for (let resultsMessage of resultsMessageLocations) {
+                    resultsMessage.classList.add('rm-active');
+                    resultsMessage.textContent = `Your search yielded ${totalResults} result(s).`;
+                }
             }
-            if (value === ' ') {
-                prev.style.display = 'block';
-            }
+        }
+        console.log(value);
+    } else {
+        totalResults = 0;
+        for (let resultsMessage of resultsMessageLocations) {
+            resultsMessage.style.display = 'none';
+        }
+        const postPreviews = document.getElementsByClassName('post-preview');
+        for (const prev of postPreviews) {
+            prev.style.display = 'block';
         }
     }
 })
